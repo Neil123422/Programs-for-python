281faf00177e61c8321baaa2150d3a022b869b03
Neil123422 <neil.s.dixit@gmail.com>
2025-01-25 19:40:00 +0530

    New files added to string

---
+++ strings/stringlen.py
@@ -0,0 +1,3 @@
+a= str(input("Enter any string: "))
+b= len(a)
+print("The length of this string is",b)
---
+++ strings/substring.py
@@ -0,0 +1,5 @@
+line= "This is a string"
+start= int(input("Enter the starting index: "))
+end= int(input("Enter the ending index: "))
+substring= line[start:end]
+print(substring)
